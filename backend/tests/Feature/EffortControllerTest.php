c604ca605384031103ac822830c658578501c445
akinoringo <wbraver@outlook.jp>
2021-05-08 15:04:06 +0900

    トップページの接続と表示のテストを追加

---
+++ backend/tests/Feature/EffortControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EffortControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testIndex(){
+        $response = $this->get(route('home'));
+        $response->assertStatus(200)
+            ->assertViewIs('home');
+
+    }
+}
