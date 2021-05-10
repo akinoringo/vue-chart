1e46a88c6bccea33258dc494dd368e85c1b49922
akinoringo <wbraver@outlook.jp>
2021-05-11 06:52:23 +0900

    efforts.searchのviewにおけるルーティングの修正

--- backend/resources/views/efforts/search.blade.php
+++ backend/resources/views/efforts/search.blade.php
@@ -1,4 +1,4 @@
-<form method="GET" action="{{route('efforts.index')}}" class="form-inline my-2 my-lg-0">
+<form method="GET" action="{{route('home')}}" class="form-inline my-2 my-lg-0">
 	@csrf
   <input class="form-control mr-sm-2" name="search" type="search" placeholder="タイトル、内容の検索" aria-label="Search">
   <button class="btn btn-outline-success py-2 my-2 my-sm-0" type="submit">検索</button>
